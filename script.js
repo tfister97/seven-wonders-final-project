9ac9402b5460994b72c6adb8f2d856acf7f7030d
Theresa Fister <tfister97@gmail.com>
2017-07-21 17:20:00 +0000

    init

---
+++ script.js
@@ -0,0 +1,527 @@
+function showChichen() {
+    //make the invisble text appear
+    var backBox = document.getElementById('backBox')
+    backBox.setAttribute("visible", true)
+    
+    var chechenItza = document.getElementById('chechenItza')
+    chichenItza.setAttribute("visible", true)
+        //make the objects disappear
+    var chichenItzaobj = document.getElementById('cIobj')
+    chichenItzaobj.setAttribute("visible", false)
+
+    var greatWallIcon = document.getElementById('greatWallIcon')
+    greatWallIcon.setAttribute("visible", false)
+
+    var tajMahalIcon = document.getElementById('tajMahalIcon')
+    tajMahalIcon.setAttribute("visible", false)
+
+    var colosseumIcon = document.getElementById('colosseumIcon')
+    colosseumIcon.setAttribute("visible", false)
+
+    var macchuPicchuIcon = document.getElementById('macchuPicchuIcon')
+    macchuPicchuIcon.setAttribute("visible", false)
+
+    var christTheRedeemerIcon = document.getElementById('CTRicon')
+    christTheRedeemerIcon.setAttribute("visible", false)
+
+
+    var petraIcon = document.getElementById('petraIcon')
+    petraIcon.setAttribute("visible", false)
+
+    //Making the cubes with text disappear 
+
+    var Chechen = document.getElementById('travelToChechen')
+    travelToChechen.setAttribute("visible", false)
+
+    var travelToTaj = document.getElementById('travelToTaj')
+    travelToTaj.setAttribute("visible", false)
+
+    var travelToCol = document.getElementById('travelToCol')
+    travelToCol.setAttribute("visible", false)
+    
+    var travelToPetra = document.getElementById('travelToPetra')
+    travelToPetra.setAttribute("visible", false)
+
+    //BOXES TO FIX
+    var travelToGreatWall = document.getElementById('travelToGreatWall')
+    travelToGreatWall.setAttribute("visible", false)
+    
+    var travelToCTR = document.getElementById('travelToCTR')
+    travelToCTR.setAttribute("visible", false)
+
+    var travelToMacchu = document.getElementById('travelToMacchu')
+    travelToMacchu.setAttribute("visible", false)
+    var music = document.getElementById("sound")
+    music.components.sound.playSound();
+    
+}
+
+function showGreatWall() {
+    //make the invisble text appear
+    var greatWall = document.getElementById('greatWall')
+    greatWall.setAttribute("visible", true)
+        //make the objects disappear
+    var chichenItzaobj = document.getElementById('cIobj')
+    chichenItzaobj.setAttribute("visible", false)
+
+    var tajMahalIcon = document.getElementById('tajMahalIcon')
+    tajMahalIcon.setAttribute("visible", false)
+
+    var colosseumIcon = document.getElementById('colosseumIcon')
+    colosseumIcon.setAttribute("visible", false)
+
+    var macchuPicchuIcon = document.getElementById('macchuPicchuIcon')
+    macchuPicchuIcon.setAttribute("visible", false)
+
+    var greatWallIcon = document.getElementById('greatWallIcon')
+    greatWallIcon.setAttribute("visible", false)
+
+    var christTheRedeemerIcon = document.getElementById('christTheRedeemerIcon')
+    var christTheRedeemerIcon = document.getElementById('CTRicon')
+    christTheRedeemerIcon.setAttribute("visible", false)
+
+
+    var petraIcon = document.getElementById('petraIcon')
+    petraIcon.setAttribute("visible", false)
+
+    //make the boxes with text disappear 
+    var travelToChechen = document.getElementById('travelToChechen')
+    travelToChechen.setAttribute("visible", false)
+
+    var travelToTaj = document.getElementById('travelToTaj')
+    travelToTaj.setAttribute("visible", false)
+
+   var travelToCol = document.getElementById('travelToCol')
+    travelToCol.setAttribute("visible", false)
+
+    var travelToCTR = document.getElementById('travelToCTR')
+    travelToTaj.setAttribute("visible", false)
+
+   var travelToPetra = document.getElementById('travelToPetra')
+    travelToPetra.setAttribute("visible", false)
+    
+     var travelToGreatWall = document.getElementById('travelToGreatWall')
+    travelToGreatWall.setAttribute("visible", false)
+    
+    var travelToCTR = document.getElementById('travelToCTR')
+    travelToCTR.setAttribute("visible", false)
+
+    var travelToMacchu = document.getElementById('travelToMacchu')
+    travelToMacchu.setAttribute("visible", false)
+    
+    var backBox = document.getElementById('backBox')
+    backBox.setAttribute("visible", true)
+
+}
+
+function showTajMahal() {
+    //make the invisble text appear
+    var tajMahal = document.getElementById('tajMahal')
+    tajMahal.setAttribute("visible", true)
+        //make the objects disappear
+    var greatWallIcon = document.getElementById('greatWallIcon')
+    greatWallIcon.setAttribute("visible", false)
+    
+     var chichenItzaobj = document.getElementById('cIobj')
+    chichenItzaobj.setAttribute("visible", false)
+
+    var tajMahalIcon = document.getElementById('tajMahalIcon')
+    tajMahalIcon.setAttribute("visible", false)
+
+    var colosseumIcon = document.getElementById('colosseumIcon')
+    colosseumIcon.setAttribute("visible", false)
+
+    var macchuPicchuIcon = document.getElementById('macchuPicchuIcon')
+    macchuPicchuIcon.setAttribute("visible", false)
+
+    var christTheRedeemerIcon = document.getElementById('CTRicon')
+    christTheRedeemerIcon.setAttribute("visible", false)
+
+
+    var petraIcon = document.getElementById('petraIcon')
+    petraIcon.setAttribute("visible", false)
+
+    //Making the cubes with text disappear 
+
+    var travelToChechen = document.getElementById('travelToChechen')
+    travelToChechen.setAttribute("visible", false)
+
+    var travelToTaj = document.getElementById('travelToTaj')
+    travelToTaj.setAttribute("visible", false)
+
+    var travelToCol = document.getElementById('travelToCol')
+    travelToCol.setAttribute("visible", false)
+    
+    var travelToPetra = document.getElementById('travelToPetra')
+    travelToPetra.setAttribute("visible", false)
+
+    //BOXES TO FIX
+    var travelToGreatWall = document.getElementById('travelToGreatWall')
+    travelToGreatWall.setAttribute("visible", false)
+    
+    var travelToCTR = document.getElementById('travelToCTR')
+    travelToCTR.setAttribute("visible", false)
+
+    var travelToMacchu = document.getElementById('travelToMacchu')
+    travelToMacchu.setAttribute("visible", false)
+    
+    var backBox = document.getElementById('backBox')
+    backBox.setAttribute("visible", true)
+}
+function showCol() {
+
+// document.getElementById("travelToCol").addEventListener("click", function(){
+     //make the invisble text appear
+    var colosseum = document.getElementById('colosseum')
+    colosseum.setAttribute("visible", true)
+        //make the objects disappear
+   var chichenItzaobj = document.getElementById('cIobj')
+    chichenItzaobj.setAttribute("visible", false)
+
+    var greatWallIcon = document.getElementById('greatWallIcon')
+    greatWallIcon.setAttribute("visible", false)
+
+    var tajMahalIcon = document.getElementById('tajMahalIcon')
+    tajMahalIcon.setAttribute("visible", false)
+
+    var colosseumIcon = document.getElementById('colosseumIcon')
+    colosseumIcon.setAttribute("visible", false)
+
+    var macchuPicchuIcon = document.getElementById('macchuPicchuIcon')
+    macchuPicchuIcon.setAttribute("visible", false)
+
+    var christTheRedeemerIcon = document.getElementById('CTRicon')
+    christTheRedeemerIcon.setAttribute("visible", false)
+
+
+    var petraIcon = document.getElementById('petraIcon')
+    petraIcon.setAttribute("visible", false)
+
+    //Making the cubes with text disappear 
+
+    var travelToChechen = document.getElementById('travelToChechen')
+    travelToChechen.setAttribute("visible", false)
+
+    var travelToTaj = document.getElementById('travelToTaj')
+    travelToTaj.setAttribute("visible", false)
+
+    var travelToCol = document.getElementById('travelToCol')
+    travelToCol.setAttribute("visible", false)
+    
+    var travelToPetra = document.getElementById('travelToPetra')
+    travelToPetra.setAttribute("visible", false)
+
+    //BOXES TO FIX
+    var travelToGreatWall = document.getElementById('travelToGreatWall')
+    travelToGreatWall.setAttribute("visible", false)
+    
+    var travelToCTR = document.getElementById('travelToCTR')
+    travelToCTR.setAttribute("visible", false)
+
+    var travelToMacchu = document.getElementById('travelToMacchu')
+    travelToMacchu.setAttribute("visible", false)
+    
+    var backBox = document.getElementById('backBox')
+    backBox.setAttribute("visible", true)
+}
+
+// function showCol(){
+//     //make the invisble text appear
+//     var colosseum = document.getElementById('colosseum')
+//     colosseum.setAttribute("visible", true)
+//         //make the objects disappear
+//     var chichenItzaobj = document.getElementById('cIobj')
+//     chichenItzaobj.setAttribute("visible", false)
+
+//     var tajMahalIcon = document.getElementById('tajMahalIcon')
+//     tajMahalIcon.setAttribute("visible", false)
+
+//     var colosseumIcon = document.getElementById('colosseumIcon')
+//     colosseumIcon.setAttribute("visible", false)
+
+//     var macchuPicchuIcon = document.getElementById('macchuPicchuIcon')
+//     macchuPicchuIcon.setAttribute("visible", false)
+
+//     var greatWallIcon = document.getElementById('greatWallIcon')
+//     greatWallIcon.setAttribute("visible", false)
+
+//     var christTheRedeemerIcon = document.getElementById('christTheRedeemerIcon')
+//     var christTheRedeemerIcon = document.getElementById('CTRicon')
+//     christTheRedeemerIcon.setAttribute("visible", false)
+
+
+//     var petraIcon = document.getElementById('petraIcon')
+//     petraIcon.setAttribute("visible", false)
+
+//     //make the boxes with text disappear 
+//     var travelToGreatWall = document.getElementById('travelToGreatWall')
+//     travelToGreatWall.setAttribute("visible", false)
+
+//     var travelToChechen = document.getElementById('travelToChechen')
+//     travelToChechen.setAttribute("visible", false)
+
+//     var travelToTaj = document.getElementById('travelToTaj')
+//     travelToTaj.setAttribute("visible", false)
+
+//     var travelToCol = document.getElementById('travelToCol')
+//     travelToTaj.setAttribute("visible", "false")
+
+//     var travelToCTR = document.getElementById('travelToCTR')
+//     travelToTaj.setAttribute("visible", false)
+
+//     var travelToMacchu = document.getElementById('travelToMacchu')
+//     travelToTaj.setAttribute("visible", false)
+
+//     var travelToPetra = document.getElementById('travelToPetra')
+//     travelToPetra.setAttribute("visible", false)
+// }
+
+function showMachu() {
+    //make the invisble text appear
+    var machuPicchu = document.getElementById('machuPicchu')
+    machuPicchu.setAttribute("visible", true)
+        //make the objects disappear
+   var chichenItzaobj = document.getElementById('cIobj')
+    chichenItzaobj.setAttribute("visible", false)
+
+    var greatWallIcon = document.getElementById('greatWallIcon')
+    greatWallIcon.setAttribute("visible", false)
+
+    var tajMahalIcon = document.getElementById('tajMahalIcon')
+    tajMahalIcon.setAttribute("visible", false)
+
+    var colosseumIcon = document.getElementById('colosseumIcon')
+    colosseumIcon.setAttribute("visible", false)
+
+    var macchuPicchuIcon = document.getElementById('macchuPicchuIcon')
+    macchuPicchuIcon.setAttribute("visible", false)
+
+    var christTheRedeemerIcon = document.getElementById('CTRicon')
+    christTheRedeemerIcon.setAttribute("visible", false)
+
+
+    var petraIcon = document.getElementById('petraIcon')
+    petraIcon.setAttribute("visible", false)
+
+    //Making the cubes with text disappear 
+
+    var travelToChechen = document.getElementById('travelToChechen')
+    travelToChechen.setAttribute("visible", false)
+
+    var travelToTaj = document.getElementById('travelToTaj')
+    travelToTaj.setAttribute("visible", false)
+
+    var travelToCol = document.getElementById('travelToCol')
+    travelToCol.setAttribute("visible", false)
+    
+    var travelToPetra = document.getElementById('travelToPetra')
+    travelToPetra.setAttribute("visible", false)
+
+    //BOXES TO FIX
+    var travelToGreatWall = document.getElementById('travelToGreatWall')
+    travelToGreatWall.setAttribute("visible", false)
+    
+    var travelToCTR = document.getElementById('travelToCTR')
+    travelToCTR.setAttribute("visible", false)
+
+    var travelToMacchu = document.getElementById('travelToMacchu')
+    travelToMacchu.setAttribute("visible", false)
+    
+    var backBox = document.getElementById('backBox')
+    backBox.setAttribute("visible", true)
+}
+function showCTR()
+{
+    //make the invisble text appear
+    var christRedeemer = document.getElementById('christRedeemer')
+    christRedeemer.setAttribute("visible", true)
+       var chichenItzaobj = document.getElementById('cIobj')
+    chichenItzaobj.setAttribute("visible", false)
+    
+    var greatWallIcon = document.getElementById('greatWallIcon')
+    greatWallIcon.setAttribute("visible", false)
+
+    var tajMahalIcon = document.getElementById('tajMahalIcon')
+    tajMahalIcon.setAttribute("visible", false)
+
+    var colosseumIcon = document.getElementById('colosseumIcon')
+    colosseumIcon.setAttribute("visible", false)
+
+    var macchuPicchuIcon = document.getElementById('macchuPicchuIcon')
+    macchuPicchuIcon.setAttribute("visible", false)
+
+    var christTheRedeemerIcon = document.getElementById('CTRicon')
+    christTheRedeemerIcon.setAttribute("visible", false)
+
+
+    var petraIcon = document.getElementById('petraIcon')
+    petraIcon.setAttribute("visible", false)
+
+    //Making the cubes with text disappear 
+
+    var travelToChechen = document.getElementById('travelToChechen')
+    travelToChechen.setAttribute("visible", false)
+
+    var travelToTaj = document.getElementById('travelToTaj')
+    travelToTaj.setAttribute("visible", false)
+
+    var travelToCol = document.getElementById('travelToCol')
+    travelToCol.setAttribute("visible", false)
+    
+    var travelToPetra = document.getElementById('travelToPetra')
+    travelToPetra.setAttribute("visible", false)
+
+    //BOXES TO FIX
+    var travelToGreatWall = document.getElementById('travelToGreatWall')
+    travelToGreatWall.setAttribute("visible", false)
+    
+    var travelToCTR = document.getElementById('travelToCTR')
+    travelToCTR.setAttribute("visible", false)
+
+    var travelToMacchu = document.getElementById('travelToMacchu')
+    travelToMacchu.setAttribute("visible", false)
+    
+    var backBox = document.getElementById('backBox')
+    backBox.setAttribute("visible", true)
+}
+
+function showPetra()
+{
+    //make the invisble text appear
+    var petra = document.getElementById("petra")
+    petra.setAttribute("visible", true)
+        //make the objects disappear
+    var chichenItzaobj = document.getElementById('cIobj')
+    chichenItzaobj.setAttribute("visible", false)
+
+    var greatWallIcon = document.getElementById('greatWallIcon')
+    greatWallIcon.setAttribute("visible", false)
+
+    var tajMahalIcon = document.getElementById('tajMahalIcon')
+    tajMahalIcon.setAttribute("visible", false)
+
+    var colosseumIcon = document.getElementById('colosseumIcon')
+    colosseumIcon.setAttribute("visible", false)
+
+    var macchuPicchuIcon = document.getElementById('macchuPicchuIcon')
+    macchuPicchuIcon.setAttribute("visible", false)
+
+    var christTheRedeemerIcon = document.getElementById('CTRicon')
+    christTheRedeemerIcon.setAttribute("visible", false)
+
+
+    var petraIcon = document.getElementById('petraIcon')
+    petraIcon.setAttribute("visible", false)
+
+    //Making the cubes with text disappear 
+
+    var travelToChechen = document.getElementById('travelToChechen')
+    travelToChechen.setAttribute("visible", false)
+
+    var travelToTaj = document.getElementById('travelToTaj')
+    travelToTaj.setAttribute("visible", false)
+
+    var travelToCol = document.getElementById('travelToCol')
+    travelToCol.setAttribute("visible", false)
+    
+    var travelToPetra = document.getElementById('travelToPetra')
+    travelToPetra.setAttribute("visible", false)
+
+    //BOXES TO FIX
+    var travelToGreatWall = document.getElementById('travelToGreatWall')
+    travelToGreatWall.setAttribute("visible", false)
+    
+    var travelToCTR = document.getElementById('travelToCTR')
+    travelToCTR.setAttribute("visible", false)
+
+    var travelToMacchu = document.getElementById('travelToMacchu')
+    travelToMacchu.setAttribute("visible", false)
+    
+    var backBox = document.getElementById('backBox')
+    backBox.setAttribute("visible", true)
+}
+
+function goBack()
+{
+    //make it go back
+    var backBox = document.getElementById('backBox')
+    backBox.setAttribute("visible", false)
+    
+    var homePage = document.getElementById('homePage')
+    homePage.setAttribute("visible", true)
+    
+    var petra = document.getElementById("petra")
+    petra.setAttribute("visible", false)
+    
+    var machuPicchu = document.getElementById('machuPicchu')
+    machuPicchu.setAttribute("visible", false)
+    
+    var colosseum = document.getElementById('colosseum')
+    colosseum.setAttribute("visible", false)
+    
+    var greatWall = document.getElementById('greatWall')
+    greatWall.setAttribute("visible", false)
+    
+    var chechenItza = document.getElementById('chechenItza')
+    chichenItza.setAttribute("visible", false)
+    
+    var christRedeemer = document.getElementById('christRedeemer')
+    christRedeemer.setAttribute("visible", false)
+    
+    
+    
+    //OBJECTS AND BOXES
+    var chichenItzaobj = document.getElementById('cIobj')
+    chichenItzaobj.setAttribute("visible", true)
+    
+    
+    var greatWallIcon = document.getElementById('greatWallIcon')
+    greatWallIcon.setAttribute("visible", true)
+
+    var tajMahalIcon = document.getElementById('tajMahalIcon')
+    tajMahalIcon.setAttribute("visible", true)
+
+    var colosseumIcon = document.getElementById('colosseumIcon')
+    colosseumIcon.setAttribute("visible", true)
+
+    var macchuPicchuIcon = document.getElementById('macchuPicchuIcon')
+    macchuPicchuIcon.setAttribute("visible", true)
+
+    var christTheRedeemerIcon = document.getElementById('CTRicon')
+    christTheRedeemerIcon.setAttribute("visible", true)
+
+
+    var petraIcon = document.getElementById('petraIcon')
+    petraIcon.setAttribute("visible", true)
+    
+    var tajMahal = document.getElementById('tajMahal')
+    tajMahal.setAttribute("visible", false)
+
+    //Making the cubes with text disappear 
+
+    var travelToChechen = document.getElementById('travelToChechen')
+    travelToChechen.setAttribute("visible", true)
+
+    var travelToTaj = document.getElementById('travelToTaj')
+    travelToTaj.setAttribute("visible", true)
+
+    var travelToCol = document.getElementById('travelToCol')
+    travelToCol.setAttribute("visible", true)
+    
+    var travelToPetra = document.getElementById('travelToPetra')
+    travelToPetra.setAttribute("visible", true)
+
+    //BOXES TO FIX
+    var travelToGreatWall = document.getElementById('travelToGreatWall')
+    travelToGreatWall.setAttribute("visible", true)
+    
+    var travelToCTR = document.getElementById('travelToCTR')
+    travelToCTR.setAttribute("visible", true)
+
+    var travelToMacchu = document.getElementById('travelToMacchu')
+    travelToMacchu.setAttribute("visible", true)
+    
+    var macchuPicchuIcon = document.getElementById('macchuPicchuIcon')
+    macchuPicchuIcon.setAttribute("visible", true)
+    
+}
